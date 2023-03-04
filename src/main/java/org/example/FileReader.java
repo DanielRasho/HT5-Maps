eb2bab6e79b40f6af4603afd3348074c61cf539a
Alfred Rayo <ray22933@uvg.edu.gt>
2023-03-04 10:57:15 -0600

    Feat: Create FileReader class for reading external files.

--- src/main/java/org/example/FileReader.java
+++ src/main/java/org/example/FileReader.java
@@ -1,2 +1,60 @@
-package org.example;public class FileReader {
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/** Provides methods to read file and store it on data structures.*/
+public class FileReader {
+    /**
+     * Reads a file and return it as an array of tokens
+     * @param file File to read.
+     * @param separator String that define where a Token end.
+     * @return List of tokens.
+     */
+    public static List<String> fileToList(File file, String separator){
+        List<String> fragments = new ArrayList<>();
+
+        // INSTANTIATING SCANNER
+        Scanner read;
+        try {
+            read = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        // SETTING SEPARATOR
+        read.useDelimiter(separator);
+
+        // FINDING FRAGMENTS
+        while (read.hasNext())
+        {
+            fragments.add(read.next());
+        }
+        read.close();
+
+        return fragments;
+    }
+
+    /**
+     * Fills a given map from a file according to given guidelines.
+     * @param file File to read.
+     * @param entriesSeparator Regex expression for separate one entry from another.
+     * @param keyValueSeparator Regex expression to separate key from value in an entry.
+     * @param map Map to fill
+     * @return Map filled with entries.
+     */
+    public static Map<String, String> fillMapFromFile(
+            File file, String entriesSeparator, String keyValueSeparator, Map<String, String> map){
+
+        List<String> entries = FileReader.fileToList(file, entriesSeparator);
+        for (String entry: entries) {
+            String[] keyValuePair = entry.split(keyValueSeparator);
+            map.put(keyValuePair[0].trim(), keyValuePair[1].trim());
+        }
+        return map;
+    }
 }
